f37206845c945e2e62027d324ccc724c77e80895
Omadbek-web <79894617+Omadbek-web@users.noreply.github.com>
2022-07-16 14:33:08 +0500

    Update script.js

--- script.js
+++ script.js
@@ -3,12 +3,10 @@ let numbers = [120, 500, 650, 950, 1200, 410, 330, 260]
 // bittasi shartni bajarsa true qaytaradi
 let x = numbers.some(x => x > 30)
 
-// hammasi shartni bajarsa true qaytaradi
-// let x = numbers.every(x => x > 0 )
 
-// let x = numbers.filter(x => {
-//     return x > 300 && x < 700
-// })
+// let x = 88
+
+let d = 110
 
 numbers.forEach(x => {
     console.log(x)  
